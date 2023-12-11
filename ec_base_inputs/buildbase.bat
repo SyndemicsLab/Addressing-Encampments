6fbeb31109dc83b2888ef17f29103e07595de480
Hana Zwick <hana.zwick@bmc.org>
2023-12-11 15:01:37 -0500

    all results calculating

--- ec_base_inputs/buildbase.bat
+++ ec_base_inputs/buildbase.bat
@@ -3,7 +3,7 @@
 
 cd ../../RESPONDv1
 
-for /l %%x in (1, 1, 5) do (
+for /l %%x in (1, 1, 1000) do (
 	Rscript ./calibration/R/ec_base.R %%x %%x
 	robocopy "input%%x" "../respond_sweeps_public/ec_base_inputs/input%%x" /s /e
 )
