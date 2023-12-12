4d292e5b04ab559db3599e0cd3bb62f4d45ee8e1
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-12-12 12:48:54 -0500

    fig script update

--- Results/Figures/SweepsFigures.R
+++ Results/Figures/SweepsFigures.R
@@ -21,20 +21,22 @@ cost <- cost[, `:=` (cost = factor(cost, levels = cost_order, ordered = TRUE),
 p1 <- ggplot(cost, aes(x = strategy, y = value, fill = cost)) + 
   geom_col(position = "stack") + 
   labs(x = "",
-       y = "Cost in USD",
+       y = "Cost (Millions USD)",
        fill = "") + 
-  scale_y_continuous(labels = scales::label_dollar()) + 
+  scale_y_continuous(labels = scales::label_dollar(scale_cut = scales::cut_short_scale())) + 
   theme_bw() +
   theme(legend.position = "bottom", legend.justification = .1) + 
   geom_text(aes(label = scales::label_dollar()(after_stat(y)), group = strategy),
-            stat = "summary", fun = "sum", hjust = .5, vjust = -.3) + 
+            stat = "summary", fun = "sum", hjust = .5, vjust = -.3,
+            size = 6) + 
   scale_fill_manual(values = c("darkgrey", colorRampPalette(colors = c("#5DD2E9", "#003866"))(4))) + 
-  coord_cartesian(ylim = c(3e6, NA))
+  coord_cartesian(ylim = c(3e6, NA)) + 
+  theme(text = element_text(size = 16))
 
 costTable <- copy(cost)[, value := scales::label_dollar()(value)
                         ][, .("Strategy" = strategy, "Cost Category" = cost, "USD" = value)
                           ][USD != "$0"
-                            ][, Strategy := factor(Strategy, levels = c("Status Quo", "Sweeps", "Housing (Choice)", "Housing (MOUD)"))]
+                            ][, Strategy := factor(Strategy, levels = c("Status Quo", "Sweeps", "Housing First", "Housing (MOUD)"))]
 p1 + ggtexttable(costTable, rows = NULL) + plot_layout(widths = c(1.8, 1))
 ggsave("cost.png", width = 16, height = 8)
 # Housing uptake ===============================================================
@@ -47,7 +49,8 @@ ggplot(uptake, aes(x = housing_uptake, y = value, col = strategy)) +
   labs(x = "Proportion taking up\n'Housing (MOUD)' from 'No Treatment'",
        y = "All Deaths",
        col = "") + 
-  theme(legend.position = "bottom") +
+  theme(legend.position = "bottom",
+        text = element_text(size = 16)) +
   scale_color_manual(values = c("#59CD90", "#3FA7D6","#773344", "#EF452E"))
 ggsave("housing_uptake.png", height = 4, width = 10)
 # Overdose Rate ================================================================
@@ -60,7 +63,8 @@ ggplot(od, aes(x = od_rate_mult, y = value, col = strategy)) +
   labs(x = "Multiplier for Default Overdose Rates\n(in Housing Only)",
        y = "Fatal Overdoses",
        col = "") + 
-  theme(legend.position = "bottom") + 
+  theme(legend.position = "bottom",
+        text = element_text(size = 16)) + 
   scale_color_manual(values = c("#59CD90", "#3FA7D6","#773344", "#EF452E")) + 
   scale_x_continuous(breaks = seq(0, 3, by = .5))
 ggsave("od_rate.png", height = 4, width = 10)  
