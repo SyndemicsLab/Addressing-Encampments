cbcda9a7b8a011cf07c216bf4178c1680b177c47
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2025-02-18 14:11:54 -0500

    Figures to svg (#5)
    * fig update
    
    * Update SweepsFigures.R
    
    * update figure SVGs
    
    * update figures
    
    * reviewer comments on fig 4
    
    * added figure titles
    
    * update on appendix figs
    
    * Delete .DS_Store
    
    * Removed link mention

--- Results/Figures/SweepsFigures.R
+++ Results/Figures/SweepsFigures.R
@@ -1,4 +1,4 @@
-if(!require(pacman)) install.packages("pacman"); pacman::p_load(data.table, ggplot2, patchwork, ggpubr, wesanderson)
+if(!require(pacman)) install.packages("pacman"); pacman::p_load(data.table, ggplot2, patchwork, ggpubr, wesanderson, svglite)
 
 # Cost =========================================================================
 cost <- fread("cost.csv")
@@ -21,14 +21,17 @@ cost <- cost[, `:=` (cost = factor(cost, levels = cost_order, ordered = TRUE),
 p1 <- ggplot(cost, aes(x = strategy, y = value, fill = cost)) + 
   geom_col(position = "stack") + 
   labs(x = "",
-       y = "Cost (Millions USD)",
-       fill = "") + 
-  scale_y_continuous(labels = scales::label_dollar(scale_cut = scales::cut_short_scale())) + 
+       y = "Cost, in millions USD",
+       fill = "",
+       title = "Figure 2. Cost of Strategies by Category", 
+       subtitle = "(1-year Time Horizon)") + 
+  scale_y_continuous(labels = scales::label_dollar(scale = 1e-6)) +
   theme_bw() +
   theme(legend.position = "bottom", legend.justification = .1) + 
-  scale_fill_manual(values = c("red", "#F38D3A", "#2A9D8F", "#60AFFF", "#E5BEED", "#264653")) + 
+  scale_fill_manual(values = c("red", "#F38D3A", "#2A9D8F", "#60AFFF", "#B6A39E", "#773344")) + 
   coord_cartesian(ylim = c(3e6, NA)) + 
   theme(text = element_text(size = 16))
+ggsave("cost_notbl.svg", height = 5, width = 9)
 
 costTable <- copy(cost)[, value := scales::label_dollar()(value)
                         ][, .("Strategy" = strategy, "Cost Category" = cost, "USD" = value)
@@ -36,20 +39,7 @@ costTable <- copy(cost)[, value := scales::label_dollar()(value)
                             ][, Strategy := factor(Strategy, levels = c("Status Quo", "Sweeps", "Housing (MOUD)", "Housing First"), ordered = TRUE)]
 setorder(costTable, Strategy)
 p1 + ggtexttable(costTable, rows = NULL) + plot_layout(widths = c(1.8, 1))
-ggsave("cost.png", width = 16, height = 8)
-
-ggplot(cost, aes(value, strategy, fill = cost)) + 
-  geom_col(position = "stack") + 
-  labs(y = "",
-       x = "Cost (Millions USD)",
-       fill = "") + 
-  scale_x_continuous(labels = scales::label_dollar(scale_cut = scales::cut_short_scale())) + 
-  theme_bw() +
-  theme(legend.position = "bottom", legend.justification = .1) + 
-  scale_fill_manual(values = c("red", "#F38D3A", "#2A9D8F", "#60AFFF", "#E5BEED", "#264653")) +
-  theme(text = element_text(size = 16))
-
-ggsave("cost_notbl.png", width = 10)
+ggsave("cost.svg", width = 16, height = 8)
 # Housing uptake ===============================================================
 uptake <- fread("housing_uptake.csv")
 
@@ -57,14 +47,14 @@ ggplot(uptake, aes(x = housing_uptake, y = value, col = strategy)) +
   geom_line() + 
   geom_point(alpha = 0.5) +
   theme_bw() + 
-  labs(x = "Proportion taking up\n'Housing (MOUD)' from 'No Treatment'",
+  labs(x = "Proportion of Encampment Population\nAccepting Housing with MOUD at the Time of the Sweep",
        y = "All Deaths",
        col = "") + 
   theme(legend.position = "bottom",
         text = element_text(size = 16)) +
   scale_color_manual(values = c("#59CD90", "#3FA7D6","#773344", "#EF452E"))
-ggsave("housing_uptake.png", height = 4, width = 10)
-# Overdose Rate ================================================================
+ggsave("housing_uptake.svg", height = 4, width = 10)
+# Overdose Rate =================================================================
 od <- fread("od_rate.csv")
 
 ggplot(od, aes(x = od_rate_mult, y = value, col = strategy)) + 
@@ -73,9 +63,11 @@ ggplot(od, aes(x = od_rate_mult, y = value, col = strategy)) +
   theme_bw() + 
   labs(x = "Multiplier for Default Overdose Rates\n(in Housing Only)",
        y = "Fatal Overdoses",
-       col = "") + 
+       col = "",
+       title = "Figure 3. Fatal Overdoses by Strategy and Housing Overdose Multiplier",
+       subtitle = "(Applied to Both Housing Strategies)") + 
   theme(legend.position = "bottom",
         text = element_text(size = 16)) + 
   scale_color_manual(values = c("#59CD90", "#3FA7D6","#773344", "#EF452E")) + 
   scale_x_continuous(breaks = seq(0, 3, by = .5))
-ggsave("od_rate.png", height = 4, width = 10)  
+ggsave("od_rate.svg", height = 4, width = 10)  
